cde032704acfc294cbc4e1ac767453630481dd15
Naqhid <mnaqhid@gmail.com>
2021-03-03 16:19:22 +0530

    modify controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
   def current_user
-    @current_user ||= User.includes(:opinions, opinions: :likes).find(session[:user_id]) if session[:user_id]
-  end
+   end
 
   helper_method :current_user
 
   def authenticate
-    flash[:alert] = 'You need to login first!' unless current_user
-    redirect_to new_session_path unless current_user
-  end
+   
 end
